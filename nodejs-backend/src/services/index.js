94d24c9da4b7742198d6e2bc47f3778c11a13c66
kanacb <kana@cloudbasha.com>
2023-09-27 16:56:08 +0800

    CodeBridge commit

---
+++ nodejs-backend/src/services/index.js
@@ -0,0 +1,14 @@
+const users = require("./users/users.service.js");
+const product = require("./product/product.service.js");
+const sheet12 = require("./sheet12/sheet12.service.js");
+const sheet1 = require("./sheet1/sheet1.service.js");
+// ~cb-add-require-service-name~
+
+// eslint-disable-next-line no-unused-vars
+module.exports = function (app) {
+  app.configure(users);
+  app.configure(product);
+  app.configure(sheet12);
+  app.configure(sheet1);
+  // ~cb-add-configure-service-name~
+};
